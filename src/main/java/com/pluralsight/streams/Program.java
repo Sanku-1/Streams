b25cb171eac481e94a5b8c7f35ce2508c260ac6a
Stephen_A <ankustephen99@gmail.com>
2025-11-04 09:48:06 -0500

    Created list of Persons and added 10 instantiated Person objects to it

--- src/main/java/com/pluralsight/streams/Program.java
+++ src/main/java/com/pluralsight/streams/Program.java
@@ -1,7 +1,45 @@
 package com.pluralsight.streams;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        List<Person> people = new ArrayList<>();
+
+        Person person1 = new Person("Stephen", "Anku", 26);
+        people.add(person1);
+        Person person2 = new Person("Steve", "Rogers", 104);
+        people.add(person2);
+        Person person3 = new Person("Clark", "Kent", 37);
+        people.add(person3);
+        Person person4 = new Person("Bruce", "Banner", 40);
+        people.add(person4);
+        Person person5 = new Person("Peter", "Parker", 19);
+        people.add(person5);
+        Person person6 = new Person("Bruce", "Wayne", 38);
+        people.add(person6);
+        Person person7 = new Person("Luffy", "Monkey", 21);
+        people.add(person7);
+        Person person8 = new Person("Sue", "Storm", 32);
+        people.add(person8);
+        Person person9 = new Person("Dick", "Grayson", 22);
+        people.add(person9);
+        Person person10 = new Person("Oliver", "Queen", 36);
+        people.add(person10);
     }
+
+        static Person search() {
+            System.out.println("Select Wh");
+
+            return null;
+        }
+
+
+
+
+
 }
